dcfe01f8449a27088effe6028f97b993caeec1d8
upndrey <upndrey@yandex.ru>
2021-02-04 18:49:45 +0700

    Готовая страница цветных металлов

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$myCurl = curl_init();
+curl_setopt_array($myCurl, array(
+    CURLOPT_URL => 'http://23met.ru/calculator.php',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query(array(/*здесь массив параметров запроса*/))
+));
+$response = curl_exec($myCurl);
+curl_close($myCurl);
+
+echo "Ответ на Ваш запрос: ".$response;
+?>
