d0f1cb49c3f70728c3fc5ee28216bdfd37ffda21
upndrey <upndrey@yandex.ru>
2021-02-16 18:17:42 +0700

    Некоторый адаптив, калькулятор и госты

--- js/price.js
+++ js/price.js
@@ -1,35 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+
     let priceSelectDom = document.querySelector(".price__select");
     let pricemenuDom = document.querySelector(".price__menu");
-    priceSelectDom.addEventListener("click", function () {
-        pricemenuDom.classList.toggle("displaynone");
-    });
-
+    if(priceSelectDom)
+        priceSelectDom.addEventListener("click", function () {
+            pricemenuDom.classList.toggle("displaynone");
+        });
+    if(window.screen.width <= 768) {
+        pricemenuDom.classList.add("displaynone");
+    }
 
     let priceSubmenuTitleDom = document.querySelectorAll(".menu__submenu");
-    priceSubmenuTitleDom.forEach(function (elem) {
-        elem.addEventListener("click", function () {
-            let submenuDom = elem.nextElementSibling;
-            submenuDom.classList.toggle("displaynone");
-            elem.querySelector(".arrDown").classList.toggle("arrDownReverse");
+    if(priceSubmenuTitleDom)
+        priceSubmenuTitleDom.forEach(function (elem) {
+            elem.addEventListener("click", function () {
+                let submenuDom = elem.nextElementSibling;
+                submenuDom.classList.toggle("displaynone");
+                elem.querySelector(".arrDown").classList.toggle("arrDownReverse");
+            });
         });
-    });
 
     let filtersOpenDom = document.querySelector(".filters__open");
     let filtersBodyDom = document.querySelector(".filters__body");
-    filtersOpenDom.addEventListener("click", function () {
-        filtersOpenDom.querySelector(".arrDown").classList.toggle("arrDownReverse");
-        filtersBodyDom.classList.toggle("displaynone");
-    });
+    if(filtersOpenDom)
+        filtersOpenDom.addEventListener("click", function () {
+            filtersOpenDom.querySelector(".arrDown").classList.toggle("arrDownReverse");
+            filtersBodyDom.classList.toggle("displaynone");
+        });
 
     let filterDom = document.querySelectorAll(".filter");
-    filterDom.forEach(function (elem) {
-        elem.addEventListener("click", function () {
-            let parent = elem.parentNode;
-            [...parent.children].forEach(function (filter) {
-                filter.classList.remove("selected")
+    if(filterDom)
+        filterDom.forEach(function (elem) {
+            elem.addEventListener("click", function () {
+                let parent = elem.parentNode;
+                [...parent.children].forEach(function (filter) {
+                    filter.classList.remove("selected")
+                });
+                elem.classList.add("selected");
             });
-            elem.classList.add("selected");
         });
-    });
+
+    let closePriceDom = document.querySelector(".menu__close");
+    if(closePriceDom)
+        closePriceDom.addEventListener("click", function () {
+            pricemenuDom.classList.toggle("displaynone");
+        });
 });
